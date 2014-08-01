ee097699f1c503da5749c49162d0992b3217fca2
Pham Thai Hoa <thaihoabo@gmail.com>
2014-08-01 17:58:08 +0700

    Install R packages
    Install R packages

---
+++ install_packages.sh
@@ -0,0 +1,10 @@
+wget http://cran.r-project.org/src/contrib/rjson_0.2.14.tar.gz
+sudo R CMD INSTALL rjson_0.2.14.tar.gz
+wget http://cran.r-project.org/src/contrib/ggplot2_1.0.0.tar.gz
+sudo R CMD INSTALL ggplot2_1.0.0.tar.gz
+wget http://cran.r-project.org/src/contrib/Cairo_1.5-6.tar.gz
+sudo R CMD INSTALL Cairo_1.5-6.tar.gz
+wget http://cran.r-project.org/src/contrib/RColorBrewer_1.0-5.tar.gz
+sudo R CMD INSTALL RColorBrewer_1.0-5.tar.gz
+wget http://cran.r-project.org/src/contrib/VennDiagram_1.6.7.tar.gz
+sudo R CMD INSTALL VennDiagram_1.6.7.tar.gz
